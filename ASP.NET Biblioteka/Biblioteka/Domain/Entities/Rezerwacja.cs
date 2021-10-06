cda389d210963f5bb958361e64e1aa9eb3c1a36b
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-10-06 19:41:44 +1000

    Dodanie klasy Rezerwacja

--- ASP.NET Biblioteka/Biblioteka/Domain/Entities/Rezerwacja.cs
+++ ASP.NET Biblioteka/Biblioteka/Domain/Entities/Rezerwacja.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Rezerwacja
+    public class Rezerwacja
     {
+        public int Id { get; set; }
+        public int KasiazkaId { get; set; }
+        public int UzytkownikId { get; set; }
+        public DateTime dataRezerwacji { get; set; }
+
     }
 }
