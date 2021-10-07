8f27a9fc784347cceffb2a0ce238813823a5a474
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-10-07 20:18:01 +1000

    Revert "Dodanie Logowania"
    This reverts commit 86c1b44e4f682f0ae13dee64d613738f947e1593.

--- ASP.NET Biblioteka/Biblioteka/Biblioteka/Models/Uzytkownik.cs
+++ ASP.NET Biblioteka/Biblioteka/Biblioteka/Models/Uzytkownik.cs
@@ -9,7 +9,6 @@ namespace Domain.Entities
 {
     public class Uzytkownik
     {
-        public int Id { get; set; }
         public string Login { get; set; }
         public string  Haslo { get; set; }
     }
