645e5213e0171f810a335a360d7986308a49f036
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-10-07 20:05:59 +1000

    Modyfikacja Logowania

--- ASP.NET Biblioteka/Biblioteka/Biblioteka/Controllers/UzytkowniksController.cs
+++ ASP.NET Biblioteka/Biblioteka/Biblioteka/Controllers/UzytkowniksController.cs
@@ -19,130 +19,39 @@ namespace Biblioteka.Controllers
             _context = context;
         }
 
-        // GET: Uzytkowniks
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Uzytkownik.ToListAsync());
-        }
-
-        // GET: Uzytkowniks/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var uzytkownik = await _context.Uzytkownik
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (uzytkownik == null)
-            {
-                return NotFound();
-            }
-
-            return View(uzytkownik);
-        }
-
         // GET: Uzytkowniks/Create
-        public IActionResult Create()
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
 
-        // POST: Uzytkowniks/Create
+      /*  // GET: Ksiazkas
+        public async Task<IActionResult> Login()
+        {
+            return View(await _context.Uzytkownik.ToListAsync());
+        }*/
+
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Login,Haslo")] Uzytkownik uzytkownik)
+        public IActionResult Loginby([Bind("Id,Login,Haslo")] Uzytkownik uzytkownik)
         {
-            if (ModelState.IsValid)
+
+            var loginIsTrue = _context.Uzytkownik.SingleOrDefault(x => x.Login == uzytkownik.Login);
+            
+            if((loginIsTrue.Haslo==uzytkownik.Haslo))
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            /*if (ModelState.IsValid)
             {
                 _context.Add(uzytkownik);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
-            }
+            }*/
             return View(uzytkownik);
-        }
-
-        // GET: Uzytkowniks/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var uzytkownik = await _context.Uzytkownik.FindAsync(id);
-            if (uzytkownik == null)
-            {
-                return NotFound();
-            }
-            return View(uzytkownik);
-        }
-
-        // POST: Uzytkowniks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Login,Haslo")] Uzytkownik uzytkownik)
-        {
-            if (id != uzytkownik.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(uzytkownik);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UzytkownikExists(uzytkownik.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(uzytkownik);
-        }
-
-        // GET: Uzytkowniks/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var uzytkownik = await _context.Uzytkownik
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (uzytkownik == null)
-            {
-                return NotFound();
-            }
-
-            return View(uzytkownik);
-        }
-
-        // POST: Uzytkowniks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var uzytkownik = await _context.Uzytkownik.FindAsync(id);
-            _context.Uzytkownik.Remove(uzytkownik);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool UzytkownikExists(int id)
