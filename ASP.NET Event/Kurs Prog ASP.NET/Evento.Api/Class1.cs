24e6d46622dc414614ddf2b034eae2ffd45eb027
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-12-27 17:11:57 +0100

    ok

---
+++ ASP.NET Event/Kurs Prog ASP.NET/Evento.Api/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Evento.Api
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ASP.NET Event/Kurs Prog ASP.NET/Evento.Core/Domain/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evento.Core.Domain
+{
+    public class User : Entity
+    {
+        public string Role { get; protected set; }
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public DateTime CreateAt { get; protected set; }
+        
+        protected User()
+        {
+
+        }
+        public User(Guid id, string role, string name, string email,string password)
+        {
+            Id = id;
+            Role = role;
+            Name = name;    
+            Email = email;
+            Password = password;
+            CreateAt = DateTime.UtcNow;
+            
+        }
+    }
+}
