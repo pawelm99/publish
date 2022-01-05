c7d562b7d5803d96aa75cdb2471315f38a639b61
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-05 18:08:04 +0100

    ok

--- ASP.NET Event/Kurs Prog ASP.NET/Kurs Prog ASP.NET/Program.cs
+++ ASP.NET Event/Kurs Prog ASP.NET/Kurs Prog ASP.NET/Program.cs
@@ -2,18 +2,52 @@ using Evento.Core.Repositories;
 using Evento.Infrastructure.Mappers;
 using Evento.Infrastructure.Repositories;
 using Evento.Infrastructure.Services;
+using Evento.Infrastructure.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+void ConfigureServices(IServiceCollection services)
+{
+    
+
+    var serviceProvider = services.BuildServiceProvider();
+    var JwTservice = serviceProvider.GetService<JwtSettings>();
+    builder.Services.AddAuthentication(o =>
+    {
+
+        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(o =>
+    {
+        //AutomaticAuthenticate = false,
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = JwTservice.Issuer,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwTservice.Key))
+        };
+    });
+}
+
+System.Configuration.Configuration config =
+               System.Configuration.ConfigurationManager.OpenExeConfiguration(
+               ConfigurationUserLevel.None) as Configuration;
 
 //builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("jwt"));
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 builder.Services.AddMvc().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.WriteIndented = true;
+    
 });
 
 builder.Services.AddScoped<IEventRepository, EventRepository>();
@@ -21,6 +55,9 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IEventService,EventServices>();
 builder.Services.AddScoped<IUserService,UserService>();
 builder.Services.AddSingleton(AutoMapperConfig.Initial());
+
+
+
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -37,9 +74,7 @@ if (!app.Environment.IsDevelopment())
  
     
 }
-/*app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Events}/{action=Index}/{id?}");*/
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -56,7 +91,8 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-//app.MapRazorPages();
+
+
 app.UseMvc();
 app.Run();
 
