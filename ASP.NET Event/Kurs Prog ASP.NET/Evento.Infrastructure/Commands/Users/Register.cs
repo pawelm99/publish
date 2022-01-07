40e74baa497c988bc363d17e9e5b494dad766c36
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-01-07 12:53:46 +0100

    ok

--- ASP.NET Event/Kurs Prog ASP.NET/Evento.Infrastructure/Commands/Users/Register.cs
+++ ASP.NET Event/Kurs Prog ASP.NET/Evento.Infrastructure/Commands/Users/Register.cs
@@ -9,9 +9,9 @@ namespace Evento.Infrastructure.Commands.Users
     public class Register
     {
         public Guid UserId { get; set; }
-        public string Email { get; protected set; }
-        public string Name { get; protected set; }
-        public string Password { get; protected set; }
-        public string Role { get; protected set; }
+        public string Email { get;  set; }
+        public string Name { get;  set; }
+        public string Password { get;  set; }
+        public string Role { get;  set; }
     }
 }
