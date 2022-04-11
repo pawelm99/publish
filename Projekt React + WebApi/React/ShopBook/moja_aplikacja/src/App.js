86cbcd627f12712b2cc8920c513f6328bb7af695
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2022-04-11 21:20:06 +0200

    git

--- Projekt React + WebApi/React/ShopBook/moja_aplikacja/src/App.js
+++ Projekt React + WebApi/React/ShopBook/moja_aplikacja/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { commerce } from './lib/commerce';
 import {Products, Navbar} from './component';
 
 const App = ()=>{
-    return(
+    const [products, setProduct] = useState([]);
+    //const [cart, setCart]
+    const fetchProducts = async () =>
+    {
+        //const {data} = await commerce.products.list();
+
+        //setProduct(data);
+    }
+
+        useEffect(()=>{
+            fetchProducts();
+        },[]);
+
+        console.log(products)
+
+        return(
     <div>
        <Navbar/>
         <Products/>
